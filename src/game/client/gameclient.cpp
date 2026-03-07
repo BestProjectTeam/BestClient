e45d765cbcb96c8927fb15b5a9fbd53b87287f87
Tater <Mr.Potatooh@gmail.com>
2026-03-06 23:59:43 -0600

    Show animated quads in entities option

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -2805,7 +2805,7 @@ void CGameClient::OnPredict()
 			HandlePredictedEvents(Tick);
 	}
 
-	if (FastInputTicks > 0) 
+	if(FastInputTicks > 0)
 	{
 		m_PredictedWorld.CopyWorld(&m_RegularPredictedWorld);
 		// m_PrevPredictedWorld.CopyWorld(&m_PrevRegularPredictedWorld); // not sure if this is worth performance cost, it seems to not matter
