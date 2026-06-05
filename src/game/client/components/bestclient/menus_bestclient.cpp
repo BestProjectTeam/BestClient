20cde8b28a703cc5de25436cc63533e6885971a6
RoflikBEST <rocer.slesh@gmail.com>
2026-06-05 03:51:51 +0300

    fix1

--- src/game/client/components/bestclient/menus_bestclient.cpp
+++ src/game/client/components/bestclient/menus_bestclient.cpp
@@ -3078,9 +3078,6 @@ void CMenus::RenderSettingsBestClient(CUIRect MainView)
 		MainView.VSplitLeft(5.0f, nullptr, &MainView);
 
 		const bool IsOnline = Client()->State() == IClient::STATE_ONLINE;
-		const bool IsFngServer = IsOnline && GameClient()->m_GameInfo.m_PredictFNG;
-		const bool Is0xFServer = IsOnline && str_comp_nocase(GameClient()->m_GameInfo.m_aGameType, "0xf") == 0;
-		const bool IsBlockedCameraServer = IsFngServer || Is0xFServer;
 
 		CUIRect LeftView, RightView;
 		MainView.VSplitMid(&LeftView, &RightView, MarginBetweenViews);
@@ -4210,7 +4207,7 @@ void CMenus::RenderSettingsBestClient(CUIRect MainView)
 			const float KeyboardExpandedHeight = LineSize * s_KeyboardPhase;
 			const float MouseExpandedHeight = (LineSize * 2.0f + MarginSmall) * s_MousePhase;
 			const float ContentHeight = LineSize + MarginSmall + LineSize + KeyboardExpandedHeight + MarginSmall + LineSize + MouseExpandedHeight;
-			CUIRect Content, Label, Button;
+			CUIRect Content, Label;
 			BeginBlock(Column, ContentHeight, Content);
 
 			Content.HSplitTop(LineSize, &Label, &Content);
